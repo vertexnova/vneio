f44b24569e2aa61081a37f3d96bf19dbf346145e
ayadav-macOs <yadav.ajeetsingh2020@gmail.com>
2026-04-05 11:07:14 -0400

    chore: Update clang-tidy configuration and clean up CHANGELOG
    - Modified the clang-tidy configuration to remove outdated references, aligning it with the current project structure.
    - Cleaned up the CHANGELOG by removing the "Unreleased" section for better clarity and organization.
    - Added author and creation date comments to asset I/O and volume test files for improved documentation.

--- tests/image/volume_test.cpp
+++ tests/image/volume_test.cpp
@@ -1,6 +1,11 @@
 /* ---------------------------------------------------------------------
  * Copyright (c) 2025 Ajeet Singh Yadav. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License")
+ *
+ * Author:    Ajeet Singh Yadav
+ * Created:   January 2026
+ *
+ * Autodoc:   yes
  * ----------------------------------------------------------------------
  */
 
